011b5f8c29049de49596ba276592b2d2c4c1bc66
Heathcliff <dennis.nathan@gmail.com>
2016-11-01 12:14:09 -0600

    removed node_modules

--- node_modules/to-int/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-var toInt = module.exports = function(value, radix, defaultValue) {
-	return parseInt(value, radix || 10) || defaultValue || 0;
-};
-
-toInt.toFloat = function(value, defaultValue) {
-	return parseFloat(value) || defaultValue || 0;
-};

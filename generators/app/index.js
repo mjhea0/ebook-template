48369f0ead7619bd23fffda93932d2eee508acf6
Heathcliff <dennis.nathan@gmail.com>
2016-11-08 10:48:37 -0700

    Added prompt in index.js to ask user for the URL of a display image they want to use. Also removed yeoman generator file as well as duplicate css file. Set max-width of .container to 950px.

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,95 +1,104 @@
-(function() {
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
 
-  'use strict';
+module.exports = yeoman.Base.extend({
+  prompting: function () {
+    this.log(yosay(
+      'Welcome to the grand ' + chalk.red('generator-ebook-template') + ' generator!'
+    ));
 
-  const yeoman = require('yeoman-generator');
-  const chalk = require('chalk');
-  const yosay = require('yosay');
-
-  module.exports = yeoman.Base.extend({
-    prompting: function() {
-      this.log(yosay(
-        'Welcome to the grand ' + chalk.red('generator-ebook-template') + ' generator!'
-      ));
-
-      const prompts = [{
-        type: 'list',
-        name: 'whichColor',
-        message: 'Which color template would you like to use?',
-        choices: ['orange', 'green', 'blue']
-      }, {
-        type: 'input',
-        name: 'title',
-        message: 'What\'s the title of the book?'
-      }];
-
-      return this.prompt(prompts).then(function(props) {
-        this.props = props;
-      }.bind(this));
-    },
-
-    writing: function() {
-      this.fs.copyTpl(
-        this.templatePath('./index.html'),
-        this.destinationPath('./index.html'),
-        {
-          title: this.props.title
+    var prompts = [{
+      type: 'list',
+      name: 'whichColor',
+      message: 'Which color template would you like to use?',
+      choices: ['orange', 'green', 'blue']
+    }, {
+      type: 'input',
+      name: 'title',
+      message: 'What\'s the title of the book?'
+    }, {
+      type: 'confirm',
+      name: 'picturePrompt',
+      message: 'Would you like to upload a book cover picture?'
+    }, {
+      when: function (response) {
+        if (response.picturePrompt) {
+          return response.picturePrompt;
+        } else {
+          response.pictureUrl = 'http://placehold.it/350x150';
         }
-      );
-      this.fs.copy(
-        this.templatePath('../assets/font'),
-        this.destinationPath('./assets/font')
-      );
-      this.fs.copy(
-        this.templatePath('../assets/ico'),
-        this.destinationPath('./assets/ico')
-      );
-      this.fs.copy(
-        this.templatePath('../assets/img'),
-        this.destinationPath('./assets/img')
-      );
-      let colors = {};
-      if (this.props.whichColor === 'orange') {
-        colors.marketingColor = '#f26922';
-        colors.socialColor = '#b0402a';
-        colors.heroColor = '#903605';
-        colors.hrColor = '#f7681a';
-        colors.aColor = '#d9563c';
-        colors.borderColor = '#c94d35';
-      } else if (this.props.whichColor === 'blue'){
-        colors.marketingColor = '#205e9e';
-        colors.socialColor = '#62d41e';
-        colors.heroColor = colors.socialColor;
-        colors.hrColor = colors.socialColor;
-        colors.aColor = colors. socialColor;
-        colors.borderColor = colors.socialColor;
-      } else if (this.props.whichColor === 'green') {
-        colors.marketingColor = '#62d41e';
-        colors.socialColor = '#ab1894';
-        colors.heroColor = colors.socialColor;
-        colors.hrColor = colors.socialColor;
-        colors.aColor = colors.socialColor;
-        colors.borderColor = colors.socialColor;
+      },
+      type: 'input',
+      name: 'pictureUrl',
+      message: 'Please input a URL.'
+    }];
+
+    return this.prompt(prompts).then(function (props) {
+      this.props = props;
+    }.bind(this));
+  },
+
+  writing: function () {
+    this.fs.copyTpl(
+      this.templatePath('./index.html'),
+      this.destinationPath('./index.html'),
+      {
+        title: this.props.title,
+        pictureUrl: this.props.pictureUrl
       }
-      this.fs.copyTpl(
-        this.templatePath('../assets/css/styles.css'),
-        this.destinationPath('./assets/css/styles.css'),
-        {
-          marketingColor: colors.marketingColor,
-          socialColor: colors.socialColor,
-          heroColor: colors.heroColor,
-          hrColor: colors.hrColor,
-          aColor: colors.aColor,
-          borderColor: colors.borderColor
-        }
-      );
+    );
+    this.fs.copy(
+      this.templatePath('../assets/font'),
+      this.destinationPath('./assets/font')
+    );
+    this.fs.copy(
+      this.templatePath('../assets/ico'),
+      this.destinationPath('./assets/ico')
+    );
+    this.fs.copy(
+      this.templatePath('../assets/img'),
+      this.destinationPath('./assets/img')
+    );
+    if (this.props.whichColor === 'orange') {
+      var marketingColor = '#f26922';
+      var socialColor = '#b0402a';
+      var heroColor = '#903605';
+      var hrColor = '#f7681a';
+      var aColor = '#d9563c';
+      var borderColor = '#c94d35';
+    } else if (this.props.whichColor === 'blue'){
+      var marketingColor = '#205e9e';
+      var socialColor = '#62d41e';
+      var heroColor = socialColor;
+      var hrColor = socialColor;
+      var aColor = socialColor;
+      var borderColor = socialColor;
+    } else if (this.props.whichColor === 'green') {
+      var marketingColor = '#62d41e';
+      var socialColor = '#ab1894';
+      var heroColor = socialColor;
+      var hrColor = socialColor;
+      var aColor = socialColor;
+      var borderColor = socialColor;
     }
+    this.fs.copyTpl(
+      this.templatePath('../assets/css/styles.css'),
+      this.destinationPath('./assets/css/styles.css'),
+      {
+        marketingColor: marketingColor,
+        socialColor: socialColor,
+        heroColor: heroColor,
+        hrColor: hrColor,
+        aColor: aColor,
+        borderColor: borderColor
+      }
+    )
+  },
 
-    // install: function() {
-    //   this.installDependencies();
-    // }
+  install: function () {
+    this.installDependencies();
+  }
 
-  });
-
-
-}());
+});

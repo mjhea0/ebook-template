e1409bebf105841f4932be5d5b9dbccb3dae70e5
Michael Herman <hermanmu@gmail.com>
2016-11-11 12:11:33 -0700

    updated generator prompts, coverted to es6

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,114 +1,120 @@
-'use strict';
-var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
-var yosay = require('yosay');
+(function() {
 
-module.exports = yeoman.Base.extend({
-  prompting: function () {
-    this.log(yosay(
-      'Welcome to the grand ' + chalk.red('generator-ebook-template') + ' generator!'
-    ));
+  'use strict';
 
-    var prompts = [{
-      type: 'list',
-      name: 'whichColor',
-      message: 'Which color template would you like to use?',
-      choices: ['orange', 'green', 'blue']
-    }, {
-      type: 'input',
-      name: 'title',
-      message: 'What\'s the title of the book?'
-    }, {
-      type: 'confirm',
-      name: 'picturePrompt',
-      message: 'Would you like to upload a book cover picture?'
-    }, {
-      when: function (response) {
-        if (response.picturePrompt) {
-          return response.picturePrompt;
-        } else {
-          response.pictureUrl = 'http://placehold.it/350x150';
+  const yeoman = require('yeoman-generator');
+  const chalk = require('chalk');
+  const yosay = require('yosay');
+
+  module.exports = yeoman.Base.extend({
+    prompting: function () {
+      this.log(yosay(
+        'Welcome to the grand ' + chalk.red('generator-ebook-template') + ' generator!'
+      ));
+      const prompts = [{
+        type: 'list',
+        name: 'whichColor',
+        message: 'Which color template would you like to use?',
+        choices: ['orange', 'green', 'blue']
+      }, {
+        type: 'input',
+        name: 'title',
+        message: 'What\'s the title of the book?',
+        default: 'Real Python'
+      }, {
+        type: 'confirm',
+        name: 'picturePrompt',
+        message: 'Would you like to upload a book cover picture?'
+      }, {
+        when: function (response) {
+          if (response.picturePrompt) {
+            return response.picturePrompt;
+          } else {
+            response.pictureUrl = 'http://placehold.it/350x150';
+          }
+        },
+        type: 'input',
+        name: 'pictureUrl',
+        message: 'Please input a URL.'
+      }, {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter a description for the book.',
+        default: 'Real Python teaches programming and web development through hands-on, interesting examples that are useful and fun! Join the thousands who have already benefited from these unique Python courses and download your copy today. Get three courses, with over 1,300 pages of content - packed with exercises, sample files, assignments, and bonus videos for only $60!'
+      }, {
+        type: 'input',
+        name: 'price',
+        message: 'What price would you like displayed for your book?',
+        default: '60'
+      }];
+      return this.prompt(prompts).then(function (props) {
+        this.props = props;
+      }.bind(this));
+    },
+    writing: function () {
+      this.fs.copyTpl(
+        this.templatePath('./index.html'),
+        this.destinationPath('./index.html'),
+        {
+          title: this.props.title,
+          pictureUrl: this.props.pictureUrl,
+          description: this.props.description,
+          price: this.props.price
         }
-      },
-      type: 'input',
-      name: 'pictureUrl',
-      message: 'Please input a URL.'
-    }, {
-      type: 'input',
-      name: 'description',
-      message: 'Please enter a description for the book.'
-    }, {
-      type: 'input',
-      name: 'price',
-      message: 'What price would you like displayed for your book?'
-    }];
-
-    return this.prompt(prompts).then(function (props) {
-      this.props = props;
-    }.bind(this));
-  },
-
-  writing: function () {
-    this.fs.copyTpl(
-      this.templatePath('./index.html'),
-      this.destinationPath('./index.html'),
-      {
-        title: this.props.title,
-        pictureUrl: this.props.pictureUrl,
-        description: this.props.description,
-        price: this.props.price
+      );
+      this.fs.copy(
+        this.templatePath('../assets/font'),
+        this.destinationPath('./assets/font')
+      );
+      this.fs.copy(
+        this.templatePath('../assets/ico'),
+        this.destinationPath('./assets/ico')
+      );
+      this.fs.copy(
+        this.templatePath('../assets/img'),
+        this.destinationPath('./assets/img')
+      );
+      var marketingColor = '';
+      var socialColor = '';
+      var heroColor = '';
+      var hrColor = '';
+      var aColor = '';
+      var borderColor = '';
+      if (this.props.whichColor === 'orange') {
+        marketingColor = '#f26922';
+        socialColor = '#b0402a';
+        heroColor = '#903605';
+        hrColor = '#f7681a';
+        aColor = '#d9563c';
+        borderColor = '#c94d35';
+      } else if (this.props.whichColor === 'blue'){
+        marketingColor = '#205e9e';
+        socialColor = '#62d41e';
+        heroColor = socialColor;
+        hrColor = socialColor;
+        aColor = socialColor;
+        borderColor = socialColor;
+      } else if (this.props.whichColor === 'green') {
+        marketingColor = '#62d41e';
+        socialColor = '#ab1894';
+        heroColor = socialColor;
+        hrColor = socialColor;
+        aColor = socialColor;
+        borderColor = socialColor;
       }
-    );
-    this.fs.copy(
-      this.templatePath('../assets/font'),
-      this.destinationPath('./assets/font')
-    );
-    this.fs.copy(
-      this.templatePath('../assets/ico'),
-      this.destinationPath('./assets/ico')
-    );
-    this.fs.copy(
-      this.templatePath('../assets/img'),
-      this.destinationPath('./assets/img')
-    );
-    if (this.props.whichColor === 'orange') {
-      var marketingColor = '#f26922';
-      var socialColor = '#b0402a';
-      var heroColor = '#903605';
-      var hrColor = '#f7681a';
-      var aColor = '#d9563c';
-      var borderColor = '#c94d35';
-    } else if (this.props.whichColor === 'blue'){
-      var marketingColor = '#205e9e';
-      var socialColor = '#62d41e';
-      var heroColor = socialColor;
-      var hrColor = socialColor;
-      var aColor = socialColor;
-      var borderColor = socialColor;
-    } else if (this.props.whichColor === 'green') {
-      var marketingColor = '#62d41e';
-      var socialColor = '#ab1894';
-      var heroColor = socialColor;
-      var hrColor = socialColor;
-      var aColor = socialColor;
-      var borderColor = socialColor;
+      this.fs.copyTpl(
+        this.templatePath('../assets/css/styles.css'),
+        this.destinationPath('./assets/css/styles.css'),
+        {
+          marketingColor: marketingColor,
+          socialColor: socialColor,
+          heroColor: heroColor,
+          hrColor: hrColor,
+          aColor: aColor,
+          borderColor: borderColor
+        }
+      );
     }
-    this.fs.copyTpl(
-      this.templatePath('../assets/css/styles.css'),
-      this.destinationPath('./assets/css/styles.css'),
-      {
-        marketingColor: marketingColor,
-        socialColor: socialColor,
-        heroColor: heroColor,
-        hrColor: hrColor,
-        aColor: aColor,
-        borderColor: borderColor
-      }
-    )
-  },
+  });
 
-  install: function () {
-    this.installDependencies();
-  }
-
-});
+}());
